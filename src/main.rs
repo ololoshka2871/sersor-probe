94bacacfe320917a983d05a62c9639d49c0bc0bc
Lolka servantov <Lolka@servant>
2024-01-12 14:18:57 +0300

    reorganize code

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@ pub mod bridge;
 mod config;
 mod devices;
 mod display_state;
+mod high_performance_clock_config_provider;
 mod hw;
 mod support;
 mod uart_macro;
 
-use bridge::ModbusBuffer;
 use defmt_rtt as _; // global logger
 use panic_probe as _;
 
@@ -23,7 +23,6 @@ use stm32f1xx_hal::gpio::{
     Alternate, GpioExt, OpenDrain, Output, PA1, PA10, PA3, PA5, PA7, PA8, PA9, PB10, PB11, PB5,
     PB6, PB7,
 };
-use stm32f1xx_hal::rcc::{HPre, PPre};
 use stm32f1xx_hal::serial::Serial;
 use stm32f1xx_hal::spi::{NoMiso, Spi, Spi1NoRemap};
 use stm32f1xx_hal::time::{Hertz, U32Ext};
@@ -37,176 +36,9 @@ use usbd_serial::CdcAcmClass;
 
 use embedded_hal::serial::{Read, Write};
 
-use support::{
-    clocking::{ClockConfigProvider, MyConfig},
-    MyLineCoding,
-};
+use support::MyLineCoding;
 
-use crate::bridge::{Builder, Execute};
-
-use hw::clock_config_48::{
-    ADC_DEVIDER, AHB_DEVIDER, APB1_DEVIDER, APB2_DEVIDER, PLL_MUL, PLL_P_DIV, USB_DEVIDER,
-};
-
-//-----------------------------------------------------------------------------
-
-struct HighPerformanceClockConfigProvider;
-
-impl HighPerformanceClockConfigProvider {
-    fn ahb_dev2val(ahb_dev: HPre) -> u32 {
-        match ahb_dev {
-            HPre::Div1 => 1,
-            HPre::Div2 => 2,
-            HPre::Div4 => 4,
-            HPre::Div8 => 8,
-            HPre::Div16 => 16,
-            HPre::Div64 => 64,
-            HPre::Div128 => 128,
-            HPre::Div256 => 256,
-            HPre::Div512 => 512,
-        }
-    }
-
-    fn apb_dev2val(apb_dev: PPre) -> u32 {
-        match apb_dev {
-            PPre::Div1 => 1,
-            PPre::Div2 => 2,
-            PPre::Div4 => 4,
-            PPre::Div8 => 8,
-            PPre::Div16 => 16,
-        }
-    }
-
-    fn pll_mul_bits(mul: u32) -> u8 {
-        (mul - 2) as u8
-    }
-
-    fn ppl_div2val(div: stm32f1xx_hal::device::rcc::cfgr::PLLXTPRE_A) -> u32 {
-        match div {
-            stm32f1xx_hal::device::rcc::cfgr::PLLXTPRE_A::Div1 => 1,
-            stm32f1xx_hal::device::rcc::cfgr::PLLXTPRE_A::Div2 => 2,
-        }
-    }
-
-    fn freeze(_acr: &mut stm32f1xx_hal::flash::ACR) -> stm32f1xx_hal::rcc::Clocks {
-        use stm32f1xx_hal::time::MHz;
-
-        let cfg = Self::to_config();
-
-        let clocks = cfg.get_clocks();
-        // adjust flash wait states
-        let acr = unsafe { &*stm32f1xx_hal::device::FLASH::ptr() };
-        unsafe {
-            acr.acr.write(|w| {
-                w.latency().bits(if clocks.sysclk() <= MHz(24) {
-                    0b000
-                } else if clocks.sysclk() <= MHz(48) {
-                    0b001
-                } else {
-                    0b010
-                })
-            })
-        }
-
-        let rcc = unsafe { &*stm32f1xx_hal::device::RCC::ptr() };
-
-        if cfg.hse.is_some() {
-            // enable HSE and wait for it to be ready
-
-            rcc.cr.modify(|_, w| w.hseon().set_bit());
-
-            while rcc.cr.read().hserdy().bit_is_clear() {}
-        }
-
-        if let Some(pllmul_bits) = cfg.pllmul {
-            // enable PLL and wait for it to be ready
-
-            #[allow(unused_unsafe)]
-            rcc.cfgr
-                .modify(|_, w| unsafe { w.pllxtpre().variant(PLL_P_DIV) });
-
-            #[allow(unused_unsafe)]
-            rcc.cfgr.modify(|_, w| unsafe {
-                w.pllmul().bits(pllmul_bits).pllsrc().bit(cfg.hse.is_some())
-            });
-
-            rcc.cr.modify(|_, w| w.pllon().set_bit());
-
-            while rcc.cr.read().pllrdy().bit_is_clear() {}
-        }
-
-        rcc.cfgr.modify(|_, w| unsafe {
-            w.adcpre().variant(cfg.adcpre);
-            w.ppre2()
-                .bits(cfg.ppre2 as u8)
-                .ppre1()
-                .bits(cfg.ppre1 as u8)
-                .hpre()
-                .bits(cfg.hpre as u8)
-                .usbpre()
-                .variant(cfg.usbpre)
-                .sw()
-                .bits(if cfg.pllmul.is_some() {
-                    // PLL
-                    0b10
-                } else if cfg.hse.is_some() {
-                    // HSE
-                    0b1
-                } else {
-                    // HSI
-                    0b0
-                })
-        });
-
-        clocks
-    }
-}
-
-impl ClockConfigProvider for HighPerformanceClockConfigProvider {
-    fn core_frequency() -> Hertz {
-        let f = crate::config::XTAL_FREQ / Self::ppl_div2val(PLL_P_DIV) * PLL_MUL
-            / Self::ahb_dev2val(AHB_DEVIDER);
-        Hertz::Hz(f)
-    }
-
-    fn apb1_frequency() -> Hertz {
-        Hertz::Hz(Self::core_frequency().to_Hz() / Self::apb_dev2val(APB1_DEVIDER))
-    }
-
-    fn apb2_frequency() -> Hertz {
-        Hertz::Hz(Self::core_frequency().to_Hz() / Self::apb_dev2val(APB2_DEVIDER))
-    }
-
-    // stm32_cube: if APB devider > 1, timers freq APB*2
-    fn master_counter_frequency() -> Hertz {
-        // TIM3 - APB1
-        if APB2_DEVIDER == PPre::Div1 {
-            Self::core_frequency()
-        } else {
-            Self::core_frequency() * 2
-        }
-    }
-
-    fn xtal2master_freq_multiplier() -> f32 {
-        PLL_MUL as f32
-            / (Self::ppl_div2val(PLL_P_DIV)
-                * Self::ahb_dev2val(AHB_DEVIDER)
-                * Self::apb_dev2val(APB2_DEVIDER)) as f32
-    }
-
-    fn to_config() -> MyConfig {
-        MyConfig {
-            hse_p_div: PLL_P_DIV,
-            hse: Some(crate::config::XTAL_FREQ),
-            pllmul: Some(Self::pll_mul_bits(PLL_MUL)),
-            hpre: AHB_DEVIDER,
-            ppre1: APB1_DEVIDER,
-            ppre2: APB2_DEVIDER,
-            usbpre: USB_DEVIDER,
-            adcpre: ADC_DEVIDER,
-        }
-    }
-}
+use crate::bridge::{Builder, Execute, ModbusBuffer};
 
 //-----------------------------------------------------------------------------
 
@@ -320,7 +152,10 @@ mod app {
             },
         );
 
-        let clocks = HighPerformanceClockConfigProvider::freeze(&mut flash.acr);
+        let clocks =
+            high_performance_clock_config_provider::HighPerformanceClockConfigProvider::freeze(
+                &mut flash.acr,
+            );
         defmt::info!("Clocks: {}", defmt::Debug2Format(&clocks));
 
         //---------------------------------------------------------------------
